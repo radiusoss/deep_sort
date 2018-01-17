5215c06c60627f563f8294aa325bf1521d89855c
Tony Zhao <yutaoz@gmail.com>
2018-01-16 17:09:01 -0800

    updated tracks to have cls_id

--- deep_sort/track.py
+++ deep_sort/track.py
@@ -63,7 +63,7 @@ class Track:
 
     """
 
-    def __init__(self, mean, covariance, track_id, n_init, max_age,
+    def __init__(self, mean, covariance, track_id, cls_id, n_init, max_age,
                  feature=None):
         self.mean = mean
         self.covariance = covariance
@@ -79,6 +79,7 @@ class Track:
 
         self._n_init = n_init
         self._max_age = max_age
+        self.cls_id = cls_id
 
     def to_tlwh(self):
         """Get current position in bounding box format `(top left x, top left y,
@@ -141,6 +142,7 @@ class Track:
 
         self.hits += 1
         self.time_since_update = 0
+        self.cls_id = detection.cls_id
         if self.state == TrackState.Tentative and self.hits >= self._n_init:
             self.state = TrackState.Confirmed
 
@@ -164,3 +166,9 @@ class Track:
     def is_deleted(self):
         """Returns True if this track is dead and should be deleted."""
         return self.state == TrackState.Deleted
+
+    def to_arr(self):
+        tmp = [self.track_id, self.cls_id]
+        tmp.extend(self.to_tlbr())
+        tmp.extend(self.mean[4:])
+        return tmp
